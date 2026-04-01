b49e968d8251074f1fd1ccce4dc9d007d9da5003
Noam Raz <noamraz8@gmail.com>
2026-04-01 13:11:16 +0300

    Finish mutex implementation
    It now uses a simple 3-state implementation that tries to avoid the more
    involved `atomic_wait`/`atomic_notify` calls in the uncontended case.

--- src/runtime/sync/mutex.c
+++ src/runtime/sync/mutex.c
@@ -1,12 +1,36 @@
 #include "mutex.h"
 
-#include "lib/assert.h"
+#include "arch/intrin.h"
+#include "core/wait.h"
+#include "lib/atomic.h"
 
-void mutex_lock_slow(mutex_t* mutex) {
-    ASSERT(!"TODO: mutex_lock_slow");
+#define SPIN_LIMIT 40
+
+void mutex_lock_slow(mutex_t* mutex, uint32_t cur_state) {
+    for (size_t i = 0;
+         i < SPIN_LIMIT && cur_state != MUTEX_STATE_LOCKED_CONTENDED; i++) {
+        cpu_relax();
+
+        // Don't issue more RfO requests until we think we might have a chance.
+        cur_state = atomic_load_relaxed(&mutex->state);
+        if (cur_state != MUTEX_STATE_UNLOCKED) {
+            continue;
+        }
+
+        if (atomic_compare_exchange_weak_acquire_relaxed(
+                &mutex->state, &cur_state, MUTEX_STATE_LOCKED)) {
+            return;
+        }
+    }
+
+    while (
+        atomic_exchange_acquire(&mutex->state, MUTEX_STATE_LOCKED_CONTENDED) !=
+        MUTEX_STATE_UNLOCKED) {
+        atomic_wait(&mutex->state, WAIT_KEY_UINT32,
+                    MUTEX_STATE_LOCKED_CONTENDED, 0);
+    }
 }
 
 void mutex_unlock_slow(mutex_t* mutex) {
-    ASSERT(!"TODO: mutex_unlock_slow");
+    atomic_notify(&mutex->state, 1);
 }
-
--- src/runtime/sync/mutex.h
+++ src/runtime/sync/mutex.h
@@ -1,63 +1,33 @@
 #pragma once
 
-#include <stdatomic.h>
+#include <stdint.h>
 
-#include "lib/defs.h"
+#include "lib/atomic.h"
 
-#define MUTEX_LOCKED_BIT BIT0
-#define MUTEX_PARKED_BIT BIT1
+enum {
+    MUTEX_STATE_UNLOCKED = 0,
+    MUTEX_STATE_LOCKED = 1,
+    MUTEX_STATE_LOCKED_CONTENDED = 2,
+};
 
 typedef struct mutex {
-    /**
-     * This atomic integer holds the current state of the mutex instance. Only the two lowest bits
-     * are used. See `LOCKED_BIT` and `PARKED_BIT` for the bitmask for these bits.
-     *
-     * State table:
-     *
-     * PARKED_BIT | LOCKED_BIT | Description
-     *     0      |     0      | The mutex is not locked, nor is anyone waiting for it.
-     * -----------+------------+------------------------------------------------------------------
-     *     0      |     1      | The mutex is locked by exactly one thread. No other thread is
-     *            |            | waiting for it.
-     * -----------+------------+------------------------------------------------------------------
-     *     1      |     0      | The mutex is not locked. One or more thread is parked or about to
-     *            |            | park. At least one of the parked threads are just about to be
-     *            |            | unparked, or a thread heading for parking might abort the park.
-     * -----------+------------+------------------------------------------------------------------
-     *     1      |     1      | The mutex is locked by exactly one thread. One or more thread is
-     *            |            | parked or about to park, waiting for the lock to become available.
-     *            |            | In this state, PARKED_BIT is only ever cleared when a bucket lock
-     *            |            | is held (i.e. in a parking_lot_core callback). This ensures that
-     *            |            | we never end up in a situation where there are parked threads but
-     *            |            | PARKED_BIT is not set (which would result in those threads
-     *            |            | potentially never getting woken up).
-     */
-    atomic_uint_fast8_t state;
+    _Atomic uint32_t state;
 } mutex_t;
 
-void mutex_lock_slow(mutex_t* mutex);
+void mutex_lock_slow(mutex_t* mutex, uint32_t cur_state);
 void mutex_unlock_slow(mutex_t* mutex);
 
 static inline void mutex_lock(mutex_t* mutex) {
-    uint8_t expected = 0;
-    if (!atomic_compare_exchange_weak_explicit(
-        &mutex->state,
-        &expected, MUTEX_LOCKED_BIT,
-        memory_order_acquire, memory_order_relaxed
-    )) {
-        mutex_lock_slow(mutex);
+    uint32_t expected = MUTEX_STATE_UNLOCKED;
+    if (!atomic_compare_exchange_weak_acquire_relaxed(&mutex->state, &expected,
+                                                      MUTEX_STATE_LOCKED)) {
+        mutex_lock_slow(mutex, expected);
     }
 }
 
 static inline void mutex_unlock(mutex_t* mutex) {
-    uint8_t exepected = MUTEX_LOCKED_BIT;
-    if (atomic_compare_exchange_strong_explicit(
-        &mutex->state,
-        &exepected, 0,
-        memory_order_release, memory_order_relaxed
-    )) {
-        return;
+    if (atomic_exchange_release(&mutex->state, MUTEX_STATE_UNLOCKED) ==
+        MUTEX_STATE_LOCKED_CONTENDED) {
+        mutex_unlock_slow(mutex);
     }
-
-    mutex_unlock_slow(mutex);
 }
