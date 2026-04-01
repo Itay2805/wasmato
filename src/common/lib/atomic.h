782c61961793c3041147b79822a040e6f4e1350f
Noam Raz <noamraz8@gmail.com>
2026-04-01 13:10:17 +0300

    Introduce a few more atomic utilities

--- src/common/lib/atomic.h
+++ src/common/lib/atomic.h
@@ -5,15 +5,28 @@
 #define atomic_fence_acquire() atomic_thread_fence(memory_order_acquire)
 #define atomic_fence_release() atomic_thread_fence(memory_order_release)
 
-#define atomic_store_relaxed(ptr, value) atomic_store_explicit(ptr, value, memory_order_relaxed)
-#define atomic_store_release(ptr, value) atomic_store_explicit(ptr, value, memory_order_release)
+#define atomic_store_relaxed(ptr, value)                                       \
+    atomic_store_explicit(ptr, value, memory_order_relaxed)
+
+#define atomic_store_release(ptr, value)                                       \
+    atomic_store_explicit(ptr, value, memory_order_release)
 
 #define atomic_load_relaxed(ptr) atomic_load_explicit(ptr, memory_order_relaxed)
 #define atomic_load_acquire(ptr) atomic_load_explicit(ptr, memory_order_acquire)
 
-#define atomic_compare_exchange_weak_relaxed(ptr, expected, desired)         \
-    atomic_compare_exchange_weak_explicit(                                   \
+#define atomic_exchange_acquire(ptr, value)                                    \
+    atomic_exchange_explicit(ptr, value, memory_order_acquire)
+
+#define atomic_exchange_release(ptr, value)                                    \
+    atomic_exchange_explicit(ptr, value, memory_order_release)
+
+#define atomic_compare_exchange_weak_relaxed(ptr, expected, desired)           \
+    atomic_compare_exchange_weak_explicit(                                     \
         ptr, expected, desired, memory_order_relaxed, memory_order_relaxed)
+
+#define atomic_compare_exchange_weak_acquire_relaxed(ptr, expected, desired)   \
+    atomic_compare_exchange_weak_explicit(                                     \
+        ptr, expected, desired, memory_order_acquire, memory_order_acquire)
 
 #define atomic_compare_exchange_strong_relaxed(ptr, expected, desired)         \
     atomic_compare_exchange_strong_explicit(                                   \
